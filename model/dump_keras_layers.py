98a5e200c3b0bc46d1a71dfe7563e5bc2d7cfb7d
Han::Mark <22847748+weikunhan@users.noreply.github.com>
2018-04-13 13:56:25 -0700

    Update dump_keras_layers.py

--- model/dump_keras_layers.py
+++ model/dump_keras_layers.py
@@ -10,3 +10,27 @@ This module dumped information from Keras model.
 # Reference: 
 ################################################################################
 """
+
+import caffe
+import numpy as np
+import os
+from keras.models import load_model
+
+layers_output = 'model/keras/layers'
+keras_model = 'model/keras/pose_iter_440000.h5'
+
+
+model = load_model(keras_model)
+
+# Loop over all layers 
+for layer in model.layers:
+      
+    # Extract each layer name
+    layer_name = layer.name
+
+    # write out weight matrices and bias vectors
+    print(layer_name, layer.get_weights()[0].shape, layer.get_weights()[1].shape)
+    np.save(os.path.join(layers_output, "W_{:s}.npy".format(k)), layer.get_weights()[0])
+    np.save(os.path.join(layers_output, "b_{:s}.npy".format(k)), layer.get_weights()[2])
+
+print("Done !")
