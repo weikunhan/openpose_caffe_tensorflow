d2a4ef96dba88442d0494463f892aa537243e35c
Han::Mark <22847748+weikunhan@users.noreply.github.com>
2018-04-12 23:31:43 -0700

    Add files via upload

---
+++ model/dump_keras_layers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+""" Dump Keras layers
+
+This module dumped information from Keras model. 
+
+################################################################################
+# Author: Weikun Han <weikunhan@gmail.com>
+# Crate Date: 04/12/2018
+# Update:
+# Reference: 
+################################################################################
+"""
