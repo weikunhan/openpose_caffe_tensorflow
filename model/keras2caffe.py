6d244f3a9c88f45663ca2b15dc94b9e5b5b0a493
Han::Mark <22847748+weikunhan@users.noreply.github.com>
2018-04-14 00:40:06 -0700

    Update keras2caffe.py

--- model/keras2caffe.py
+++ model/keras2caffe.py
@@ -8,7 +8,7 @@ Keras model.
 ################################################################################
 # Author: Weikun Han <weikunhan@gmail.com>
 # Crate Date: 04/12/2018
-# Update:
+# Update: 04/14/2018
 # Reference: 
 ################################################################################
 """
@@ -19,36 +19,36 @@ import caffe
 
 # Please modify input path to locate you file
 KERAS_LAYERS_DIR = 'keras/layers'
-CAFFE_INPUT_DIR = 'caffe/'
-CAFFE_OUTPUT_DIR = 'caffe/pose_iter_440000_convert.caffemodel'
+CAFFE_DIR = 'caffe'
 
-# Get the skeleton of the Caffe model
-model_filename = 'pose_deploy.prototxt'
-model = os.path.join(CAFFE_INPUT_DIR, model_filename)
+# Setup input and output name
+caffe_proto_filename = 'pose_deploy.prototxt'
+caffe_model_filename = 'pose_iter_440000_convert.caffemodel'
 
 # Setup the Caffe model
-net = caffe.Net(model, caffe.TEST)
+caffe_proto = os.path.join(CAFFE_DIR, caffe_proto_filename)
+net = caffe.Net(caffe_proto, caffe.TEST)
 
 # Loop over all layers 
-for layer_name, variable in net.params.items():
+for name, param in net.params.items():
     
     # Set all weights and biases from Caffe model into Keras model 
-    if (os.path.exists(os.path.join(KERAS_LAYERS_DIR, 
-                                    'w_{:s}.npy'.format(layer_name))):
+    if os.path.exists(os.path.join(KERAS_LAYERS_DIR, 
+                                   'w_{:s}.npy'.format(name))):
         w = np.load(os.path.join(KERAS_LAYERS_DIR, 
-                                 'w_{:s}.npy'.format(layer_name)))
+                                 'w_{:s}.npy'.format(name)))
         b = np.load(os.path.join(KERAS_LAYERS_DIR, 
-                                 'b_{:s}.npy'.format(layer_name)))
+                                 'b_{:s}.npy'.format(name)))
         
-        # TODO: do we need transpose for caffe2keras?
         # Tranfer the right form to store
-        #w = np.transpose(w, (2, 3, 1, 0))
+        w = np.transpose(w, (3, 2, 0, 1))
         
         # Store into Keras model
-        net.params[layer_name][0].data[...] = w
-        net.params[layer_name][1].data[...] = b
+        net.params[name][0].data[...] = w
+        net.params[name][1].data[...] = b
 
 # Output Caffe model
-net.save(CAFFE_OUTPUT_DIR)
+caffe_model = os.path.join(CAFFE_DIR, caffe_model_filename)
+net.save(caffe_model)
 
 print('-------------------------Finished converting---------------------------')
