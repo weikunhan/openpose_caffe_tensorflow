cb47fbb324176047bd2bdaf3c7ca4ec379ed84cc
Han::Mark <22847748+weikunhan@users.noreply.github.com>
2018-04-15 17:28:16 -0700

    Update dump_keras_layers.py

--- model/dump_keras_layers.py
+++ model/dump_keras_layers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """ Dump Keras layers
+
 This module dumped information from Keras model. 
+
 ################################################################################
 # Author: Weikun Han <weikunhan@gmail.com>
 # Crate Date: 04/12/2018
