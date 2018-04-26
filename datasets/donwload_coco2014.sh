6cee93dac5f2aaf6ef14f9c16881758f2c497f18
Han::Mark <22847748+weikunhan@users.noreply.github.com>
2018-04-25 19:51:32 -0700

    Update donwload_coco2014.sh

--- datasets/donwload_coco2014.sh
+++ datasets/donwload_coco2014.sh
@@ -4,9 +4,12 @@
 # Note: Better use google chrome download, it is faster (and mamually unzip)!
 
 mkdir train_coco2014
-mkdir validation_coco2014
+mkdir val_coco2014
 mkdir test_coco2014
 mkdir annotations_coco2014
+
+echo "Downloading annotations datasets 2014..."
+wget http://msvocds.blob.core.windows.net/annotations-1-0-3/person_keypoints_trainval2014.zip
 
 echo "Downloading train datasets 2014..."
 #wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
@@ -17,11 +20,8 @@ echo "Downloading validation datasets 2014..."
 echo "Downloading test datasets 2014..."
 #wget http://msvocds.blob.core.windows.net/coco2014/test2014.zip
 
-echo "Downloading annotations datasets 2014..."
-wget http://msvocds.blob.core.windows.net/annotations-1-0-3/person_keypoints_trainval2014.zip
-
 echo "Unzipping annotations datasets 2014..."
-unzip person_keypoints_trainval2014.zip -d annotations_coco2014
+unzip person_keypoints_trainval2014.zip 
 
 echo "Unzipping test datasets 2014..."
 #unzip test2014.zip -d test_coco2014
@@ -32,7 +32,16 @@ echo "Unzipping validation datasets 2014..."
 echo "Unzipping train datasets 2014..."
 #unzip train2014.zip -d train_coco2014
 
+mv -v annotations/* annotations_coco2014/
+#mv -v train2014/* train_coco2014/
+#mv -v val2014/* val_coco2014/
+#mv -v test2014/* test_coco2014/
 rm -f person_keypoints_trainval2014.zip
-#rm -f test2014.zip
+rm -rf annotations
 #rm -f train2014.zip
+#rm -rf train2014
 #rm -f val2014.zip
+#rm -rf val2014
+#rm -f test2014.zip
+#rm -rf test2014
+
